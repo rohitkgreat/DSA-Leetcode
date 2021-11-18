8def6b56f6956f7410567895f19fbce0f2b7973b
Rohit Kumar <56231663+rohitkr06@users.noreply.github.com>
2021-11-18 12:32:18 +0530

    Time: 4 ms (70.47%), Space: 8.6 MB (12.62%) - LeetHub

---
+++ permutations/permutations.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        int n=nums.size();
+        int arr[n+1];
+        
+        for(int i=0;i<n;i++)
+            arr[i]=0;
+        
+        vector<vector<int>> ans;
+        vector<int> temp;
+        
+        helper(nums,ans,temp,arr,n,0);
+        
+        return ans;
+        
+    }
+    
+    void helper(vector<int> nums,vector<vector<int>> &ans,vector<int> temp,int arr[],int n,int idx)
+    {
+        if(temp.size()==n)
+        {
+            ans.push_back(temp);
+            return;
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==0)
+            {
+                temp.push_back(nums[i]);
+                arr[i]=1;
+                helper(nums,ans,temp,arr,n,i+1);
+                arr[i]=0;
+                temp.pop_back();
+            }
+        }
+        
+        return;
+    }
+};

62c6a4c63380875016b9329c6c0450f3cb05747d
Rohit Kumar <56231663+rohitkr06@users.noreply.github.com>
2022-06-13 20:11:44 +0530

    Time: 3 ms (42.66%), Space: 6.2 MB (52.36%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    
+    int helper(int row,int col,int m,int n,int **dp)
+    {
+        if(row==m-1 && col==n-1)
+            return 1;
+        if(row>=m || col>=n)
+            return 0;
+        
+        if(dp[row][col]!=-1)
+            return dp[row][col];
+        
+        int right=helper(row,col+1,m,n,dp);
+        int down=helper(row+1,col,m,n,dp);
+        
+        
+        dp[row][col]= right+down;
+        return dp[row][col];
+        
+    }
+    int uniquePaths(int m, int n) {
+        
+        int **dp=new int*[m];
+        
+      
+        for(int i=0;i<m;i++)
+        {
+            dp[i]=new int [n];
+            for(int j=0;j<n;j++)
+                dp[i][j]=-1;
+        }
+        return helper(0,0,m,n,dp);
+//         int dp[101][101];
+//         for(int i=0;i<m;i++)
+//             for(int j=0;j<n;j++)
+//             {
+//                 dp[i][j]=1;
+//             }
+        
+//         for(int i=1;i<m;i++)
+//             for(int j=1;j<n;j++)
+//                 dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            
+        
+//         return dp[m-1][n-1];
+    }
+};

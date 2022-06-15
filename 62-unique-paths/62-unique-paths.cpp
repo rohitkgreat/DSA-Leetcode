565d878bbdc9ed95ca9b2a3c71838b1d8daa7523
Rohit Kumar <56231663+rohitkr06@users.noreply.github.com>
2022-06-15 11:28:09 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (52.34%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -30,7 +30,33 @@ public:
             for(int j=0;j<n;j++)
                 dp[i][j]=-1;
         }
-        return helper(0,0,m,n,dp);
+        
+        
+        // int z= helper(0,0,m,n,dp);
+        // for(int i=0;i<m;i++)
+        // {for(int j=0;j<n;j++)
+        //     cout<<dp[i][j]<<" ";
+        // cout<<endl;
+        // }
+        // return z;
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                dp[i][j]=1;
+            }
+        }
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+                dp[i][j]=dp[i][j-1]+dp[i-1][j];
+        }
+        
+        return dp[m-1][n-1];
+        
+        
+        
 //         int dp[101][101];
 //         for(int i=0;i<m;i++)
 //             for(int j=0;j<n;j++)

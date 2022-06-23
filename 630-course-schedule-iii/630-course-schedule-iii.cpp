b3caa48a3df4a394ea6cecd58cd0e144cebb695e
Rohit Kumar <56231663+rohitkr06@users.noreply.github.com>
2022-06-23 12:25:25 +0530

    Time: 441 ms (30.16%), Space: 56.3 MB (41.31%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    static bool compare(vector<int>a,vector<int>b){
+    static bool compare(vector<int>&a,vector<int>&b){
         if(a[1]==b[1]){
             return a[0]<b[0];
         }
@@ -13,27 +13,13 @@ public:
         int last=0;
         int n=arr.size();
         for(int i=0;i<n;i++){
-            int dur=arr[i][0];
-            int end=arr[i][1];
-            int temp=last+dur;
-            if(end>=temp){
-                ans++;
-                last=temp;
-                pq.push(dur);
-            }else{
-                if(pq.size()){
-                    int temp1=pq.top();
-                    if(dur<temp1){
-                        int temp2=last-temp1+dur;
-                        if(temp2<=end){
-                            pq.pop();
-                            last=temp2;
-                            pq.push(dur);
-                        }
-                    }
-                }
+            last+=arr[i][0];
+            pq.push(arr[i][0]);
+            if(last>arr[i][1]){
+                last-=pq.top();
+                pq.pop();
             }
         }
-        return ans;
+        return pq.size();
     }
 };

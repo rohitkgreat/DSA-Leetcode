f341edc8846af8673254fa0df95c514f6337ab9d
Rohit Kumar <56231663+rohitkr06@users.noreply.github.com>
2022-06-30 20:15:23 +0530

    Time: 399 ms (5.40%), Space: 7.5 MB (65.12%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -1,42 +1,20 @@
 class Solution {
 public:
-    int helper(int start,int end,vector<int>nums,bool turn,int **dp)
-    {
-        if(start>end)
-            return 0;
-        
-        if(dp[start][end]!=-1)
-            return dp[start][end];
-        
-        int play1;
-        if(turn)
-       { play1=max(helper(start+1,end,nums,false,dp)+nums[start],helper(start,end-1,nums,false,dp)+nums[end]);
-         return play1; 
-       }
-        
-        int play2=min(helper(start+1,end,nums,true,dp),helper(start,end-1,nums,true,dp));
-        
-        dp[start][end]=play2;
-        return play2;    
+    bool helper(vector<int>& nums,int start,int end,int turn,int scorea,int scoreb){
+        if(start>end){
+            return (scorea>=scoreb);
+        }
+        if(turn==0){
+            bool temp=helper(nums,start+1,end,1-turn,scorea+nums[start],scoreb);
+            bool x= helper(nums,start,end-1,1-turn,scorea+nums[end],scoreb);
+            return temp||x;
+        }else{
+            bool temp=helper(nums,start+1,end,1-turn,scorea,scoreb+nums[start]);
+            bool y=helper(nums,start,end-1,1-turn,scorea,scoreb+nums[end]);
+            return y&&temp;
+        }
     }
     bool PredictTheWinner(vector<int>& nums) {
-        int n=nums.size()-1;
-        int sum=0;
-        
-        int **dp=new int*[n+1];
-        for(int i=0;i<=n;i++)
-        {
-            dp[i]=new int[n+1];
-            for(int j=0;j<=n;j++)
-                dp[i][j]=-1;
-        }
-        
-        
-        for(auto z:nums)
-            sum+=z;
-        int x= helper(0,n,nums,true,dp);
-        int y=sum-x;
-        
-        return x>=y;
+        return helper(nums,0,nums.size()-1,0,0,0);
     }
 };

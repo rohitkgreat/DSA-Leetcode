f957f28bf9f44dfb82c3cb6d2cf32528ec315051
Rohit Kumar <56231663+rohitkr06@users.noreply.github.com>
2022-07-04 12:07:29 +0530

    Time: 35 ms (46.56%), Space: 18.1 MB (33.66%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -6,11 +6,10 @@ public:
         vector<int> ans(n,1);
         vector<int> ans2(n,1);
         
-        for(int i=0;i<ratings.size()-1;i++)
+        for(int i=0;i<n-1;i++)
         {
             if(ratings[i]<ratings[i+1])
-                ans[i+1]=ans[i]+1;
-       
+                ans[i+1]=ans[i]+1;       
         }
         for(int i=n-1;i>0;i--)
         {
@@ -18,12 +17,11 @@ public:
                ans2[i-1]=ans2[i]+1;
         }
         int sum=0;
-        for(int i=0;i<n;i++)
-        {
-           
-            sum+=max(ans[i],ans2[i]);
-        }
-//         
+        
+        for(int i=0;i<n;i++)           
+        sum+=max(ans[i],ans2[i]);
+        
+   
         return sum;
             
     }

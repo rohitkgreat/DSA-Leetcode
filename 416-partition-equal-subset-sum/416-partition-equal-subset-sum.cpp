25f36eba5ec920e7221c467d0dc5c7ea65e1bd26
Rohit Kumar <56231663+rohitkr06@users.noreply.github.com>
2022-07-20 12:54:55 +0530

    Time: 428 ms (31.22%), Space: 74.2 MB (32.25%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -18,7 +18,7 @@ public:
         for(auto x:nums)
             sum+=x;
         
-        if(sum%2!=0)
+        if(sum%2!=0 || nums.size()==1)
             return false;
         // cout<<sum;
         int n=nums.size();
@@ -29,8 +29,27 @@ public:
         {
             dp[i]=new int[sum+1];
             for(int j=0;j<=sum;j++)
-                dp[i][j]=-1;
+                {dp[i][j]=0;
+                dp[i][0]=1;}
+            
         }
-        return helper(0,sum,nums,dp);
+        if(nums[0]>sum)
+            return false;
+        dp[0][nums[0]]=1;
+        
+        for(int i=1;i<n;i++)
+            for(int j=1;j<=sum;j++)
+            {
+                if(nums[i]>sum)
+                    return false;
+                bool a=false;
+                if(j>=nums[i])
+                a=dp[i-1][j-nums[i]];
+                bool b=dp[i-1][j];
+                
+                dp[i][j]=a||b;
+            }
+        // return helper(0,sum,nums,dp);
+        return dp[n-1][sum];
     }
 };

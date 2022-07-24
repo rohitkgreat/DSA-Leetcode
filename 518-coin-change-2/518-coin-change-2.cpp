933f71afc70164307666fb9988bb816ed5037506
Rohit Kumar <56231663+rohitkr06@users.noreply.github.com>
2022-07-24 11:07:42 +0530

    Time: 51 ms (38.97%), Space: 18.5 MB (36.19%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    int helper(int pos,int amt,vector<int>& coins,int **dp)
+    int helper(int i,int target,vector<int> &coins,int **dp)
     {
-        if(pos>=coins.size() || amt<0)
-            return 0;
-        if(dp[amt][pos]!=-1)
-           return dp[amt][pos];
-        if(amt==0)
+        if(target==0)
             return 1;
-      
-        int ans=0;
+        if(i>=coins.size() || target<0)
+            return 0;
+        if(dp[i][target]!=-1)
+            return dp[i][target];
         
-        for(int i=pos;i<coins.size();i++)
-        {
-           ans+=helper(i,amt-coins[i],coins,dp);
-           
-        }
-        dp[amt][pos]=ans;
-        return ans;
+        int x=helper(i+1,target,coins,dp);
+        int y=helper(i,target-coins[i],coins,dp);
+        
+        dp[i][target]=x+y;
+        return x+y;
     }
-    
     int change(int amount, vector<int>& coins) {
-        int **dp=new int*[amount+1];
-        for(int i=0;i<amount+1;i++)
+        int n=coins.size();
+        int **dp=new int*[n];
+        
+        for(int i=0;i<n;i++)
         {
-            dp[i]=new int[coins.size()];
-            for(int j=0;j<coins.size();j++)
+            dp[i]=new int[amount+1];
+            for(int j=0;j<=amount;j++)
                 dp[i][j]=-1;
-        }
             
+        }
         
         return helper(0,amount,coins,dp);
     }

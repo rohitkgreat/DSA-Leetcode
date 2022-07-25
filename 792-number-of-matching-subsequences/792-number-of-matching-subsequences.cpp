c10ef330fa52f7e7a2d86fb4cdf9aa60ab45880a
Rohit Kumar <56231663+rohitkr06@users.noreply.github.com>
2022-07-25 12:08:33 +0530

    Time: 203 ms (89.91%), Space: 37.2 MB (89.10%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,30 +1,46 @@
 class Solution {
 public:
-    bool issubsequence(string& s1, string& s2){
-        int n = s1.length(), m = s2.length();
-        int i = 0, j = 0;
-        while (i < n && j < m) {
-            if (s1[i] == s2[j])
-                i++;
-            j++;
-        }
-        return i == n;
+    bool helper(int i,int j,string &s,string &t)
+    {
+         if(j>=t.size())
+            return true;
+        
+        if(i>=s.size())
+            return false;
+       
+        if(t[j]==s[i])
+            return helper(i+1,j+1,s,t);
+        return helper(i+1,j,s,t);
     }
     int numMatchingSubseq(string s, vector<string>& words) {
+        
         int ans=0;
-        map<string,int>map;
-        for(int i=0;i<words.size();i++){
-            if(map.count(words[i])>0){
-                ans+=map[words[i]];
-                continue;
-            }
-            if(issubsequence(words[i],s)){
-                map[words[i]]=1;
-                ans++;
-            }else{
-                map[words[i]]=0;
-            }
+        int n=s.size();
+        unordered_map<string,int>mp;
+        
+        for(int i=0;i<words.size();i++)
+        {
+            if(mp.count(words[i])>0)
+                {ans+=mp[words[i]];
+                 continue;
+                }
+            int m=words[i].size();
+//             int **dp=new int*[n];
+//             for(int i=0;i<n;i++)
+//             {
+//                 dp[i]=new int[m];
+//                 for(int j=0;j<m;j++)
+//                     dp[i][j]=-1;
+//             }
+            
+            if(helper(0,0,s,words[i]))
+                {ans++;
+                 mp[words[i]]=1;
+                }
+            else
+                mp[words[i]]=0;
         }
+        
         return ans;
     }
 };

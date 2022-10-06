6167394b30c1404bcb3de7f03f12b022b6a10066
Rohit Kumar <56231663+rohitkr06@users.noreply.github.com>
2022-10-06 11:28:15 +0530

    Time: 4 ms (40.89%), Space: 7.8 MB (63.44%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,46 +1,24 @@
 class Solution {
 public:
-//     int helper(int i,int n,vector<int>& nums,int *dp)
-//     {
-//         if(i>=n)
-//             return 0;
-//         if(dp[i]!=-1)
-//             return dp[i];
-//         int first=-1,second=-1;
-//         if(i!=n-1)
-//         first=helper(i+2,n,nums,dp)+nums[i];
-        
-//         second=helper(i+1,n,nums,dp);
-        
-//         dp[i]= max(first,second);
-//         return dp[i];
-    // }
-    int helper2(int i,int n,vector<int>& nums,int *dp)
-    {
+    int helper(int i,int n,vector<int>& nums,vector<int> &dp){
         if(i>=n)
             return 0;
+        
         if(dp[i]!=-1)
             return dp[i];
-        dp[i]=max(helper2(i+2,n,nums,dp)+nums[i],helper2(i+1,n,nums,dp));
-        return dp[i];
+        
+        int x=nums[i]+helper(i+2,n,nums,dp);
+        int y=0+helper(i+1,n,nums,dp);
+        
+        return dp[i]=max(x,y);
     }
-    
     int rob(vector<int>& nums) {
         int n=nums.size();
-        int *dp=new int[n];
-        for(int i=0;i<n;i++)
-            dp[i]=-1;
         if(n==1)
             return nums[0];
-        
-        int ans1=helper2(0,n-1,nums,dp);
-        for(int i=0;i<n;i++)
-            dp[i]=-1;
-        
-        int ans2=helper2(1,n,nums,dp);
-    
-        return max(ans1,ans2);
-        // ans=max(ans,nums[n-1]);
-        // return ans;
+        vector<int> dp(n+1,-1);
+        int ans=helper(0,n-1,nums,dp);
+        fill(dp.begin(),dp.end(),-1);
+        return max(ans,helper(1,n,nums,dp));
     }
 };
